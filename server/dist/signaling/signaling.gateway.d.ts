63d3b6cd3c861ca1e8daff0e43e732c0092744bb
luffy977338 <timur.djeletyan.2009@gmail.com>
2024-10-06 22:27:13 +0300

    n

--- server/dist/signaling/signaling.gateway.d.ts
+++
@@ -1,7 +0,0 @@
-import { Server } from 'socket.io';
-export declare class SignalingGateway {
-    server: Server;
-    handleOffer(data: any): void;
-    handleAnswer(data: any): void;
-    handleCandidate(data: any): void;
-}
